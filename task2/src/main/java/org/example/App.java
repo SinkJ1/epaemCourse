beec6bbb889d4efc3695d2aa6fb0d420a0fb5039
SinkJ <boi54357@gmail.com>
2019-11-08 16:53:49 +0300

    read from file

--- task2/src/main/java/org/example/App.java
+++ task2/src/main/java/org/example/App.java
@@ -6,6 +6,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,21 +17,11 @@ import java.util.regex.Pattern;
  */
 public class App 
 {
-    String str = "";
+    private static final String path = "textFiles/text.txt";
 
     public static void main( String[] args ) throws IOException {
-        String str = "Hello 123124 -8.8 GJj, sdkkds. sldkfj 1.";
         TextBuild te = new TextBuild();
-        te.getText("E:\\rep\\epamCourse\\task2\\src\\main\\java\\resources\\TextFlle.xls");
-
-        File file = new File("textFiles/text.txt");
-        FileOutputStream fileOutputStream = new FileOutputStream(file);
-
-        String greetings = "Привет! Добро пожаловать на JavaRush - лучшй сайт для тех, кто хочет стать программистом!";
-
-        //fileOutputStream.write(greetings.getBytes());
-
-        //fileOutputStream.close();
-
+        te.getText(path);
+        te.getSentenceList().stream().forEach(System.out::println);
     }
 }
--- task2/src/main/java/text/TextBuild.java
+++ task2/src/main/java/text/TextBuild.java
@@ -2,29 +2,54 @@ package text;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class TextBuild {
 
     private static final Logger logger = LogManager.getLogger(TextBuild.class);
+    private List<String> sentenceList = new ArrayList<>();
+    private char charArray[];
 
-    public String getText(String path){
+    public List<String> getText(String path){
 
+        List<String> sentenceList = new ArrayList<>();
 
-        String textArray = "";
-
-        try(FileInputStream fis = new FileInputStream("textFiles/text.txt")){
-            System.out.println("fis.available()");
-        }catch (IOException e){
-            logger.warn(e);
-        System.out.println("-------------><");
+        try{
+            Files.lines(Paths.get(path), StandardCharsets.UTF_8).forEach(line -> lineParser(line));
+        }catch (IOException ex){
+            logger.warn(ex);
         }
-
-        return textArray;
+        return sentenceList;
     }
 
+    private void addInArray(){
+        
+    }
+
+
+    private void lineParser(String line){
+
+        StringBuilder stringBuilder = new StringBuilder();
+        charArray = line.toCharArray();
+
+        for(int i = 0; i < charArray.length - 1;i++){
+            stringBuilder.append(charArray[i]);
+            if(((charArray[i] == '.' || charArray[i] == '!' || charArray[i] == '?') && charArray[i+1] == ' ') || i+2 == charArray.length){
+                sentenceList.add(stringBuilder.toString());
+                i++;
+               stringBuilder.setLength(0);
+            }
+        }
+    }
+
+    public List<String> getSentenceList(){
+        return sentenceList;
+    }
 
 }
