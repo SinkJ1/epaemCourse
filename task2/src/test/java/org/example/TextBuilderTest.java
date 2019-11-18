e71ca3698d669ec4a9219f55cfe582735fb20030
SinkJ <boi54357@gmail.com>
2019-11-18 23:11:45 +0300

    add more tests

--- task2/src/test/java/org/example/TextBuilderTest.java
+++ task2/src/test/java/org/example/TextBuilderTest.java
@@ -17,14 +17,36 @@ public class TextBuilderTest {
     @Test
     public void replaceWordTest(){
         TextBuilder tb = new TextBuilder();
+        List<Element> elementList = new ArrayList<>();
 
-        List<Sentence> elementList = new ArrayList<>();
+        elementList.add(new Element(elementType.word,"Hello",5,false));
+        elementList.add(new Element(elementType.other,",",1,false));
+        elementList.add(new Element(elementType.blank," ",1,false));
+        elementList.add(new Element(elementType.word,"World",5,false));
+        elementList.add(new Element(elementType.punctuationMark,"!",1,true));
 
-        elementList.add(new Sentence());
+        Sentence sentence = new Sentence();
 
-        List<Sentence> newElementList = new ArrayList<>();
+        sentence.setElementList(elementList);
+        List<Sentence> sentenceList = new ArrayList<>();
+        sentenceList.add(sentence);
 
-        Assert.assertEquals(tb.replaceWord(2,3,"new Word",elementList), newElementList);
+
+        elementList.add(new Element(elementType.word,"new Word",5,false));
+        elementList.add(new Element(elementType.other,",",1,false));
+        elementList.add(new Element(elementType.blank," ",1,false));
+        elementList.add(new Element(elementType.word,"new Word",5,false));
+        elementList.add(new Element(elementType.punctuationMark,"!",1,true));
+
+        Sentence newSentence = new Sentence();
+
+        newSentence.setElementList(elementList);
+        List<Sentence> newSentenceList = new ArrayList<>();
+        newSentenceList.add(sentence);
+
+
+
+        Assert.assertEquals(tb.replaceWord(0,5,"new Word",sentenceList), newSentenceList);
 
     }
 
